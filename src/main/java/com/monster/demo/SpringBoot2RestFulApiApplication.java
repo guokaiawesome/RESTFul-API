574869d2d04b908e6fbfa292723b9e1b880af195
guokai <383219558@qq.com>
2018-07-15 12:33:31 +0800

    program skeleton

---
+++ src/main/java/com/monster/demo/SpringBoot2RestFulApiApplication.java
@@ -0,0 +1,12 @@
+package com.monster.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2RestFulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2RestFulApiApplication.class, args);
+	}
+}
