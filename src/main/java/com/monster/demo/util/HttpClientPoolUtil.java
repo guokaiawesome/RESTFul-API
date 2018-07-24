fa90739962ef0ace08e789749bc2f47549e6c6ef
guokai <383219558@qq.com>
2018-07-24 09:59:04 +0800

    mybatis support generate java8 time

--- src/main/java/com/monster/demo/util/HttpClientPoolUtil.java
+++ src/main/java/com/monster/demo/util/HttpClientPoolUtil.java
@@ -1,8 +1,18 @@
 package com.monster.demo.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.Header;
+import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeader;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 public class HttpClientPoolUtil {
 	
@@ -12,7 +22,24 @@ public class HttpClientPoolUtil {
 		cm.setMaxTotal(200);
 		cm.setDefaultMaxPerRoute(20);
 		
-		CloseableHttpClient httpClient=HttpClients.custom().setConnectionManager(cm).build();
+		HttpClientBuilder httpClientBuilder=HttpClients.custom();
+		httpClientBuilder.setConnectionManager(cm);
+		httpClientBuilder.setRetryHandler(new DefaultHttpRequestRetryHandler(2, true));
+		
+		List<Header> defaultHeaders=new ArrayList<>();
+		defaultHeaders.add(new BasicHeader("User-Agent", ""));
+		defaultHeaders.add(new BasicHeader("", ""));
+		httpClientBuilder.setDefaultHeaders(defaultHeaders);
+		
+		
+		//CloseableHttpClient httpClient=HttpClients.custom().setConnectionManager(cm).build();
+		HttpClient httpClient=httpClientBuilder.build();
+		
+		new HttpComponentsClientHttpRequestFactory(httpClient);
+		
+		RestTemplate template=new RestTemplate();
+		//template.setRequestFactory(requestFactory);
+		
 		
 	}
 
