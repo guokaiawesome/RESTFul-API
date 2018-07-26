57dca5c37a92f5fb8c1a2c42a20acf3099af7075
guokai <383219558@qq.com>
2018-07-26 19:56:40 +0800

    logback modify

--- src/main/java/com/monster/demo/aop/ResponseTimeAspect.java
+++ src/main/java/com/monster/demo/aop/ResponseTimeAspect.java
@@ -1,5 +1,7 @@
 package com.monster.demo.aop;
 
+import java.time.Instant;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -20,7 +22,8 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class ResponseTimeAspect {
 	
-	private static final Logger logger=LoggerFactory.getLogger(ResponseTimeAspect.class);
+	//看下通过这种方式能不能获取xml中配置的特定的某类日志
+	private static final Logger logger=LoggerFactory.getLogger("response_time");
 
 	/**
 	 * @description   public 表示public 修饰的方法
@@ -36,8 +39,20 @@ public class ResponseTimeAspect {
 	public Object outputLogAndExecute(ProceedingJoinPoint jp) throws Throwable {
 		//这里的异常处理需要关注一下，实际测试一下，和统一异常处理的先后顺序
 		
+		long start=Instant.now().toEpochMilli();
 		//这一步必须要有返回值，否则controller返回不了任何值
 		Object obj=jp.proceed();
+		
+		jp.getTarget().getClass().getName();
+		jp.getSignature().getName();
+		jp.getSignature().getDeclaringType().getName();
+		jp.getSourceLocation().toString();
+		
+		long end=Instant.now().toEpochMilli();
+		logger.info("接口调用时间={} 毫秒",end-start);
+
+		//打印调用时间日志，这个最好单独放在一个文件当中，和info error那些区分开
+		//看看能不能
 		
 		return obj;
 	}
